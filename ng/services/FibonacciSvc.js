0e7928054c69c1238abe0d49e5d92679b2000a67
Brad Beltowski <bbelowski@fullsail.com>
2016-09-08 18:18:12 -0400

    Getting ready for Demo

--- ng/services/FibonacciSvc.js
+++ ng/services/FibonacciSvc.js
@@ -1,19 +1,19 @@
 angular.module('fireApp')
 .service('FibonacciSvc', function() {
-  this.looping = function(n) {
-    var a = 0, b = 1, f = 1;
-    for(var i = 2; i <= n; i++) {
-      f = a + b;
-      a = b;
-      b = f;
-    }
-    return f;
-  };
-  this.recursive = function(n) {
-    if(n <= 2) {
-      return 1;
-    } else {
-      return this.recursive(n-1) + this.recursive(n-2)
-    }
-  };
+  // this.looping = function(n) {
+  //   var a = 0, b = 1, f = 1;
+  //   for(var i = 2; i <= n; i++) {
+  //     f = a + b;
+  //     a = b;
+  //     b = f;
+  //   }
+  //   return f;
+  // };
+  // this.recursive = function(n) {
+  //   if(n <= 2) {
+  //     return 1;
+  //   } else {
+  //     return this.recursive(n-1) + this.recursive(n-2)
+  //   }
+  // };
 });
