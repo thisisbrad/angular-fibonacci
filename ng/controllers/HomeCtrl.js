d332657dc98e3b6bd07ee259b72aafccf3e0e01d
Brad Beltowski <bbelowski@fullsail.com>
2016-09-15 22:21:44 -0400

    making FIbonacci Loop

--- ng/controllers/HomeCtrl.js
+++ ng/controllers/HomeCtrl.js
@@ -5,13 +5,13 @@ angular.module('fireApp')
 	$scope.fibonacciSequence = [];
 
 	// Function that creates Fibonacci numbers with looping
-	// $scope.printFibonacciLooping = function() {
-	// 	for(var i = 2; i <= 15; i++) {
-	// 		var num = FibonacciSvc.looping(i)
-	// 		console.info("Fibonacci: ", num)
-	// 		$scope.fibonacciSequence.push(num)
-	// 	}
-	// }
+	$scope.printFibonacciLooping = function() {
+		for(var i = 2; i <= 15; i++) {
+			var num = FibonacciSvc.looping(i)
+			console.info("Fibonacci: ", num)
+			$scope.fibonacciSequence.push(num)
+		}
+	}
 
 	// Function that creates Fibonacci numbers with recursion
 // 	$scope.printFibonacciRecursive = function() {
--- ng/services/FibonacciSvc.js
+++ ng/services/FibonacciSvc.js
@@ -1,14 +1,14 @@
 angular.module('fireApp')
 .service('FibonacciSvc', function() {
-  // this.looping = function(n) {
-  //   var a = 0, b = 1, f = 1;
-  //   for(var i = 2; i <= n; i++) {
-  //     f = a + b;
-  //     a = b;
-  //     b = f;
-  //   }
-  //   return f;
-  // };
+  this.looping = function(n) {
+    var a = 0, b = 1, f = 1;
+    for(var i = 2; i <= n; i++) {
+      f = a + b;
+      a = b;
+      b = f;
+    }
+    return f;
+  };
   // this.recursive = function(n) {
   //   if(n <= 2) {
   //     return 1;
